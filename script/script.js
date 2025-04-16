e1dc2ab86eb20e2c7b4b0525763d6a73b4eae1fe
Vinicius Fahl <viniciusfahlfrei@hotmail.com>
2025-04-16 19:34:03 -0300

    insert images and fixing styles

--- script/script.js
+++ script/script.js
@@ -1,6 +1,8 @@
 const audio = document.getElementById('meuAudio');
 const botao = document.getElementById('botaoPlayPause');
 const barra = document.getElementById('barraProgresso');
+const tempoAtual = document.getElementById('tempoAtual');
+const tempoTotal = document.getElementById('tempoTotal');
 
 botao.addEventListener('click', () => {
   if (audio.paused) {
@@ -8,6 +10,10 @@ botao.addEventListener('click', () => {
   } else {
     audio.pause();
   }
+});
+
+audio.addEventListener('loadedmetadata', () => {
+  tempoTotal.textContent = formatarTempo(audio.duration);
 });
 
 audio.addEventListener('play', () => {
@@ -21,6 +27,7 @@ audio.addEventListener('pause', () => {
 audio.addEventListener('timeupdate', () => {
   const percent = (audio.currentTime / audio.duration) * 100;
   barra.style.width = percent + '%';
+  tempoAtual.textContent = formatarTempo(audio.currentTime);
 });
 
 function mudarProgresso(e) {
@@ -28,4 +35,10 @@ function mudarProgresso(e) {
   const cliqueX = e.offsetX;
   const tempo = (cliqueX / largura) * audio.duration;
   audio.currentTime = tempo;
+}
+
+function formatarTempo(segundos) {
+  const min = Math.floor(segundos / 60);
+  const seg = Math.floor(segundos % 60);
+  return `${String(min).padStart(2, '0')}:${String(seg).padStart(2, '0')}`;
 }
